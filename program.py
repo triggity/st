9a56750e4a2ef75b3809c18f1c8224b619b1a32e
michael truong <truongmt@gmail.com>
2014-02-24 22:30:21 -0800

    into transactions

--- program.py
+++ program.py
@@ -5,9 +5,50 @@ import parsebdi
 
 pp = pprint.PrettyPrinter()
 
+def unique_name(routing, acct):
+    return routing + acct
+def unique_to_parts(name):
+    return name[:9], name[9:]
+def net_transaction(key, value):
+    routing, account = unique_to_parts(key)
+    output = {
+        "routing_number": routing,
+        "account_number": account,
+        "net_transactions": value
+    }
+    return output
+def ledger(transactions):
+    accounts = {}
+    for trx in transactions:
+        if trx:
+            if (trx["type"] == "Credit"):
+              recipient = trx["recipient"] 
+              origin = trx["originator"] 
+            else:
+              recipient = trx["originator"] 
+              origin = trx["recipient"] 
+            rname = unique_name(*recipient.split(" / "))
+            oname = unique_name(*origin.split(" / "))
+            if oname not in accounts:
+                accounts[oname] = 0
+            if rname not in accounts:
+                accounts[rname] = 0
+            accounts[rname] += int(trx["amount"])
+            accounts[oname] -= int(trx["amount"])
+    output = [net_transaction(key, value) for (key, value) in accounts.iteritems()]
+    return output
+
 def main():
-    return parsebdi.to_dict()
-    dicted = to_dict()
-    return dicted
+    dicted = parsebdi.to_dict()
+    net = ledger(dicted['transactions'])
+    output = {
+        "batch": dicted["batch"],
+        "description": dicted["description"],
+        "accounts": net
+    }
+    pp.pprint(output)
+    return output
+
+
 if __name__ == "__main__":
-    pp.pprint(main())
+    main()
